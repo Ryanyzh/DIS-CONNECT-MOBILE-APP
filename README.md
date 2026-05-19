c7773b84f026f6a4a15f75cf54dbcba460324db8
Ryanyzh <ryanyong1621@gmail.com>
2026-05-19 16:04:38 +0800

    include miscellaneous files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DIS-CONNECT-MOBILE-APP
 Used for Orbital Project, this is a mobile application that allows scholars to submit and track enquiries.
+
---
+++ android/app/src/main/kotlin/com/example/disconnect_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.disconnect_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
